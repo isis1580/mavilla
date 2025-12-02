5dd4c8141d817e1ce926ffd43e501ebcd0e32a72
Isis Cissé <zoomper2017@gmail.com>
2025-12-02 11:24:29 +0100

    Add requirements.txt

--- villana/villana/settings.py
+++ villana/villana/settings.py
@@ -88,12 +88,12 @@ WSGI_APPLICATION = 'villana.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.parse(os.getenv("DATABASE_URL", f"sqlite:///{BASE_DIR / 'db.sqlite3'}"))
 }
+
 
 
 # Password validation
