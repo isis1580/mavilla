a8891664ea8663c6834332d9b484f02642dbe1e2
Isis Cissé <zoomper2017@gmail.com>
2026-02-13 00:10:49 +0100

    add build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 # build.sh
 
-set -o errexit
-
+echo "🚀 Installation des dépendances..."
 pip install -r requirements.txt
 
+echo "📦 Collecte des fichiers statiques..."
 python manage.py collectstatic --noinput
-python manage.py migrate
+
+echo "✅ Build terminé !"
