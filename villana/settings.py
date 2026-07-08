bcc2cff42c590411b42987176ad146aeb28cc0ac
Ismael Cisse <itsupport.niamey@noomhotels.com>
2026-07-08 10:24:24 +0100

    Correction erreur 500 : suppression du Middleware inexistant SimpleDebugMiddleware

--- villana/settings.py
+++ villana/settings.py
@@ -77,7 +77,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'maison.views.SimpleDebugMiddleware',
 ]
 # =========================
 # URLS & TEMPLATES
